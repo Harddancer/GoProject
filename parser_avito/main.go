a3e95e6eaaa1411994573260ff780aaef986c9b3
Андронов Михаил <mvandronov@www.mts.ru>
2022-10-12 00:26:39 +0300

    add regxp

--- parser_avito/main.go
+++ parser_avito/main.go
@@ -3,31 +3,46 @@ package main
 import (
 	"fmt"
 	"parseravito/paser"
+	"regexp"
 	"strings"
 )
 
 func main() {
-	channel := paser.Grab()
-	// type Avto struct {
-	// 	number int
-	// 	vendor string
-	// 	// model  string
-	// 	// year   string
-	// }
-	// var auto Avto
-	// var ch []string
-	s := <-channel
-	// ch = strings.Split(s, " ")
-	fmt.Print(strings.TrimSpace(s))
-	//fmt.Println(ch)
-	// for _, v := range ch {
-	// 	v2 := strings.Split(v, " ")
-	// 	// auto.number = i
-	// 	// auto.vendor = v2[0]
-	// 	// auto.model = v2[1]
-	// 	// auto.year = v2[2]
-	// 	fmt.Println(v2)
 
-	// }
+	channel := paser.Grab()
+	s := <-channel
+	var auto Avto
+	ch := strings.Split(s, ",")
+	auto.Printing(ch)
+
+}
+
+type Avto struct {
+	number int
+	vendor string
+	model  string
+	year   string
+}
+
+func (a *Avto) Printing(ch []string) {
+	for i, v := range ch {
+
+		re := regexp.MustCompile("[0-9]+")
+		y := strings.Join(re.FindAllString(v, 1), "")
+		a.number = i
+		a.year = y
+
+		re2 := regexp.MustCompile("[a-zA-Z]+\\s")
+		vend := strings.Join(re2.FindAllString(v, 1), "")
+		a.vendor = vend
+
+		v = strings.TrimPrefix(v, " ")
+
+		re3 := regexp.MustCompile("\\s+[a-x,A-Z,а-я,0-9,-,()]+")
+		mod := strings.Join(re3.FindAllString(v, 1), "")
+		a.model = mod
+
+		fmt.Println(a.number, a.year, a.vendor, a.model)
+	}
 
 }
